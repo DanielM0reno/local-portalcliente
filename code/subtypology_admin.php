ce80aed5d3cac1f5d1a6ebc41135d1c0492101b6
DanielM0reno <121643580+DanielM0reno@users.noreply.github.com>
2023-05-01 20:23:35 +0200

    Update subtypology_admin.php

--- code/subtypology_admin.php
+++ code/subtypology_admin.php
@@ -54,33 +54,64 @@
                             
                             echo '<div class="alert alert-success">La subtipología ha sido eliminada correctamente.</div>';
                         }
-									
+								
+                        // VARIABLES para DOC_TYPE
+                        $doc1 = [
+                            "title" => "",
+                            "description" => ""
+                        ];
+                        $doc2 = [
+                            "title" => "",
+                            "description" => ""
+                        ];
+                        $doc3 = [
+                            "title" => "",
+                            "description" => ""
+                        ];
+
                         // Mostramos formulario
 						if ( $wact == 'add' || $wact == 'edit' ){
-							
+                            
+
 							if ( $wact == 'edit' ){
 							
 								$query = "SELECT * FROM INCIDENT_SUBTYPE WHERE ID = '".$wid."' AND STATUS = 'A'";
 								$res = ejecuta( $query );														
 								$row_user = $res->fetch_assoc();
-								
+
+                                $query2 = "SELECT 	`DOC`, 	`DOCITEM_TITLE`, `DOCITEM_DESCRIPTION` FROM `doc_type` 
+                                WHERE FK_SUBTYPE = ".$wid.";";
+								$res2 = ejecuta( $query2 );			
+
+                                while ( $row = $res2->fetch_assoc())
+                                {
+                                    if($row['DOC'] == 1){
+                                        $doc1['title'] = $row['DOCITEM_TITLE'];
+                                        $doc1['description'] = $row['DOCITEM_DESCRIPTION'];
+                                    }elseif ($row['DOC'] == 2){
+                                        $doc2['title'] = $row['DOCITEM_TITLE'];
+                                        $doc2['description'] = $row['DOCITEM_DESCRIPTION'];
+                                    }elseif ($row['DOC'] == 3){
+                                        $doc3['title'] = $row['DOCITEM_TITLE'];
+                                        $doc3['description'] = $row['DOCITEM_DESCRIPTION'];
+                                    }
+                                }
 							}
 							
 					?>
-
+                            <form class="" action="<?php echo action('','op=admin_subtipologia&act=save'); ?>" method="POST">
 							<div class="row">
+
                                 <div class="col-lg-6">
                                     <div class="card">
                                         <div class="card-body">
             
                                             <h4 class="mt-0 header-title">Edición de subtipología</h4>
                                             
-            
-                                            <form class="" action="<?php echo action('','op=admin_subtipologia&act=save'); ?>" method="POST">
                                             <input type="hidden" name="id" value="<?php echo $row_user['ID']; ?>">
                                                 
-												 <div class="form-group">
-                                                 <label for="">Seleccione tipología: </label>
+                                                <div class="form-group">
+                                                <label for="">Seleccione tipología: </label>
 
                                                     <!-- Selector de tipologia -->
                                                     <select class="form-control w-50 " name="tipologia" id="sel_tipologia">
@@ -118,21 +149,20 @@
                                                                 placeholder="" name="nombre" value="<?php echo $row_user['NAME']; ?>"/>
                                                     </div>
                                                 </div>
-																																	
-												
+                                                                                                                                    
+                                                
                                                 <div class="form-group mb-0">
                                                     <div>
                                                         <button type="submit" class="btn btn-primary waves-effect waves-light">
                                                             Guardar
                                                         </button>
                                                         <a href="<?php echo action('','op=admin_subtipologia'); ?>">
-															<button type="button" class="btn btn-secondary waves-effect m-l-5">
+                                                            <button type="button" class="btn btn-secondary waves-effect m-l-5">
                                                             Cancelar
-															</button>
-														</a>
+                                                            </button>
+                                                        </a>
                                                     </div>
                                                 </div>
-                                            </form>
             
                                         </div>
                                     </div>
@@ -143,10 +173,10 @@
                                     <div class="card" id="doc1-form">
                                         <div class="card-body">
                                             <label for="doc1-textarea">TÍtulo para observaciones</label>
-                                            <input type="text" class="form-control" id="doc1-input" placeholder="Introduce un titulo de asunto" required>
+                                            <input type="text" class="form-control" id="doc1-input"  value="<?php echo $doc1['title'];?>" required>
                                             <small id="help" class="form-text text-muted">Debes de introducir un titulo para identificar el caso.</small>
                                             <label for="doc1-textarea">Descripción a aportar en incidencia</label>
-                                            <textarea class="form-control"  name="doc1" id="doc1-textarea" cols="20" required></textarea>
+                                            <textarea class="form-control"  name="doc1" id="doc1-textarea" cols="20" required><?php echo $doc1['description'];?></textarea>
                                         </div>
                                     </div>
                                     <hr>
@@ -162,24 +192,25 @@
                                     <div class="collapse" id="doc2">
                                         <div class="card card-body">
                                             <label for="doc2-input">TÍtulo para imagenes</label>
-                                            <input type="text" class="form-control" id="doc2-input" placeholder="Introduce un titulo de imagen">
+                                            <input type="text" class="form-control" id="doc2-input" value="<?php echo $doc2['title'];?>">
                                             <small id="help" class="form-text text-muted">Debes de introducir un titulo para identificar la imagen.</small>
                                             <label for="doc2-textarea">Descripción a aportar en incidencia para imagen</label>
-                                            <textarea class="form-control"  name="doc2" id="doc2-textarea" cols="20"></textarea>
+                                            <textarea class="form-control"  name="doc2" id="doc2-textarea" cols="20"><?php echo $doc2['description'];?></textarea>
                                         </div>
                                     </div>
                                     <div class="collapse" id="doc3">
                                         <div class="card card-body">
                                             <label for="doc3-input">TÍtulo para documento</label>
-                                            <input type="text" class="form-control" id="doc3-input" placeholder="Introduce un titulo de documento">
+                                            <input type="text" class="form-control" id="doc3-input" value="<?php echo $doc3['title'];?>">
                                             <small id="help" class="form-text text-muted">Debes de introducir un titulo para identificar el documento.</small>
                                             <label for="doc3-textarea">Descripción a aportar en incidencia para documento</label>
-                                            <textarea class="form-control"  name="doc3" id="doc3-textarea" cols="20"></textarea>
+                                            <textarea class="form-control"  name="doc3" id="doc3-textarea" cols="20"><?php echo $doc3['description'];?></textarea>
                                         </div>
                                     </div>
                                 </div>
+                                
                             </div> <!-- end row -->
-
+                            </form>
 					
 					<?php
 							
